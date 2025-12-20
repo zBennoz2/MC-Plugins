3d71cb23c3ee7092f63933d1425cddac5d3b6251
zBennoz2 <bennosgaming@outlook.com>
2025-12-20 18:59:12 +0100

    Add chat-based rank management

--- Server-Plugin/src/main/java/com/zbennoz/zbencore/ZBenCore.java
+++ Server-Plugin/src/main/java/com/zbennoz/zbencore/ZBenCore.java
@@ -2,19 +2,27 @@ package com.zbennoz.zbencore;
 
 import com.zbennoz.zbencore.branding.BrandingService;
 import com.zbennoz.zbencore.commands.AboutCommand;
+import com.zbennoz.zbencore.commands.RankCommand;
 import com.zbennoz.zbencore.commands.ZBenCommand;
 import com.zbennoz.zbencore.listeners.PlayerJoinListener;
+import com.zbennoz.zbencore.ranks.RankConversationManager;
+import com.zbennoz.zbencore.ranks.RankService;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ZBenCore extends JavaPlugin {
 
     private BrandingService brandingService;
+    private RankService rankService;
+    private RankConversationManager rankConversations;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
 
         this.brandingService = new BrandingService(this);
+        this.rankService = new RankService(this);
+        this.rankService.load();
+        this.rankConversations = new RankConversationManager(this, rankService);
 
         // Commands
         var aboutCmd = getCommand("about");
@@ -29,8 +37,16 @@ public final class ZBenCore extends JavaPlugin {
             zbenCmd.setTabCompleter(zben);
         }
 
+        var rankCmd = getCommand("rank");
+        if (rankCmd != null) {
+            var rc = new RankCommand(this, rankService, rankConversations);
+            rankCmd.setExecutor(rc);
+            rankCmd.setTabCompleter(rc);
+        }
+
         // Listener
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this, brandingService), this);
+        getServer().getPluginManager().registerEvents(rankConversations, this);
 
         // Apply tablist to online players on reload
         if (getConfig().getBoolean("features.tablistBranding", true)) {
